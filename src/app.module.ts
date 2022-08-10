ea5f17687fbd56932553c1274b893e530722e587
linuxlite <eze.rey92@gmail.com>
2022-08-10 15:41:53 -0300

    modulos invitado terminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SpotifyService } from './spotify/spotify.service';
-import { SpotifyController } from './spotify/spotify.controller';
 import { SpotifyModule } from './spotify/spotify.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { InvitadoService } from './invitado/invitado.service';
-import { InvitadoController } from './invitado/invitado.controller';
 import { InvitadoModule } from './invitado/invitado.module';
 
 @Module({
@@ -15,7 +11,7 @@ import { InvitadoModule } from './invitado/invitado.module';
     SpotifyModule,
     InvitadoModule,
   ],
-  controllers: [AppController, InvitadoController],
-  providers: [AppService, InvitadoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/invitado/invitado.module.ts
+++ src/invitado/invitado.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { InvitadoController } from './invitado.controller';
+import { InvitadoService } from './invitado.service';
+import { InvitadoSchema } from './schemas/invitado.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Invitados', schema: InvitadoSchema }]),
+  ],
+  controllers: [InvitadoController],
+  providers: [InvitadoService],
+})
 export class InvitadoModule {}
