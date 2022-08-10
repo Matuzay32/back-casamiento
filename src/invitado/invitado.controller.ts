708c6bbfee876c77e6ac0eb40a5be9fb866b6b5b
linuxlite <eze.rey92@gmail.com>
2022-08-10 15:59:27 -0300

    invitado mayormente creado

--- src/invitado/invitado.controller.ts
+++ src/invitado/invitado.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get, Post } from '@nestjs/common';
+import { InvitadoService } from './invitado.service';
+import { InterfaceInvitado } from '../invitado/interfaces/invitado.interface';
+import { CreateInvitadoDto } from './dto/invitado.dto';
 
 @Controller('invitado')
-export class InvitadoController {}
+export class InvitadoController {
+  constructor(private readonly invitadoService: InvitadoService) {}
+  @Get()
+  mostrarTodosLosInvitados(): Promise<InterfaceInvitado[]> {
+    return this.invitadoService.mostrarTodosLosInvitados();
+  }
+  @Post()
+  cargarInvitadoCofirmado(
+    @Body() invitado: CreateInvitadoDto,
+  ): Promise<InterfaceInvitado> {
+    return this.invitadoService.cargarInvitadoCofirmado(invitado);
+  }
+}
--- src/invitado/invitado.service.ts
+++ src/invitado/invitado.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateInvitadoDto } from './dto/invitado.dto';
+import { InterfaceInvitado } from './interfaces/invitado.interface';
 @Injectable()
-export class InvitadoService {}
+export class InvitadoService {
+  constructor(
+    @InjectModel('Invitados') private invitadoModel: Model<InterfaceInvitado>,
+  ) {}
+
+  async mostrarTodosLosInvitados(): Promise<any> {
+    try {
+      return await this.invitadoModel.find({});
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: `No se encontraron invitados`,
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+
+  async cargarInvitadoCofirmado(
+    invitado: CreateInvitadoDto,
+  ): Promise<InterfaceInvitado> {
+    try {
+      return await this.invitadoModel.create(invitado);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: `Fue imposible crear el invitado `,
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+}
