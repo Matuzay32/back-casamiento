a8ca9680d86ec1a56e97b2645ce251ba8a6d1e32
linuxlite <eze.rey92@gmail.com>
2022-08-22 12:05:15 -0300

    haciendo el registro de usuarios

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+
+export const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    // match: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+  },
+  email: {
+    type: String,
+    require: true,
+    match: /.+\@.+\..+/,
+  },
+
+  password: {
+    type: String,
+    require: true,
+  },
+
+  // cars: [{ type: Schema.Types.ObjectId, ref: 'Cars' }],
+});
