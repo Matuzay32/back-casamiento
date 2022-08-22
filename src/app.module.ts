ce7fc62377ac49c4e6870d98f0f06b5a32498f4c
linuxlite <eze.rey92@gmail.com>
2022-08-22 18:51:58 -0300

    aplicando el middleware de admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpotifyModule } from './spotify/spotify.module';
@@ -10,6 +15,10 @@ import { FotoCentralService } from './foto-central/foto-central.service';
 import { FotoCentralController } from './foto-central/foto-central.controller';
 import { FotoCentralModule } from './foto-central/foto-central.module';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { SpotifyController } from './spotify/spotify.controller';
+import { InvitadoController } from './invitado/invitado.controller';
+import { FechaController } from './fecha/fecha.controller';
 
 @Module({
   imports: [
@@ -26,23 +35,24 @@ import { UsersModule } from './users/users.module';
 })
 
 // DESPUES TENGO QUE PROBARLO CON EL TOKEN
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(LoggerMiddleware)
-//       .exclude(
-//         { path: 'cars', method: RequestMethod.GET },
-//         'cars/findAll',
-//         { path: 'cars', method: RequestMethod.GET },
-//         'cars/findAll/name',
-//         { path: 'cars', method: RequestMethod.GET },
-//         'cars/findOneForId/(.*)',
-//         { path: 'cars', method: RequestMethod.GET },
-//         'cars/uploads/(.*)',
-//         { path: 'cars', method: RequestMethod.POST },
-//         'cars/files',
-//       )
-//       .forRoutes(CarsController);
-//   }
-// }
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'spotify', method: RequestMethod.POST },
+        { path: 'invitado', method: RequestMethod.POST },
+        { path: 'fecha', method: RequestMethod.POST },
+        // { path: 'cars', method: RequestMethod.GET },
+        // 'cars/findAll/name',
+        // { path: 'cars', method: RequestMethod.GET },
+        // 'cars/findOneForId/(.*)',
+        // { path: 'cars', method: RequestMethod.GET },
+        // 'cars/uploads/(.*)',
+        // { path: 'cars', method: RequestMethod.POST },
+        // 'cars/files',
+      )
+      .forRoutes(SpotifyController, InvitadoController, FechaController);
+  }
+}
+// export class AppModule {}
